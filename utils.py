133c95bb8f300103f8d3a582f4133eb871bb2ec3
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2016-04-08 14:31:38 +0100

    Added todo

--- utils.py
+++ utils.py
@@ -8,7 +8,8 @@ log = logging.getLogger(__name__)
 # http://stackoverflow.com/questions/9446387/how-to-retry-urllib2-request-when-fails
 @retry(urllib2.URLError, tries=4, delay=3, backoff=2)
 def urlopen_with_retry(url):
-    return urllib2.urlopen(url)
+    return urllib2.urlopen(url)  #TODO: 404 should be handled differently
+
 
 def query_uniprot(search_terms, first=False):
     """
