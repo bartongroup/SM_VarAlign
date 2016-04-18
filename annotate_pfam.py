3f56381fb89cf9901bfe97cb5bfed6e65608a91c
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2016-04-18 16:31:57 +0100

    First working commit (includes hard-coded Pfam AC filter)

--- annotate_pfam.py
+++ annotate_pfam.py
@@ -1,28 +1,43 @@
+import annotate_alignment
 import argparse
-from Bio import AlignIO, SeqIO
+from Bio import AlignIO, SeqIO  # Needs PR #768 #769 patched
 import logging
 
 log = logging.getLogger(__name__)
 
 
-def main(args):
+def main(local_uniprot, local_pfam, seq_id_filter, use_local_alignment, downloads):
     """
 
     :param args:
     :return:
     """
-    swissprot = SeqIO.index(args.local_uniprot, 'swiss')
-    pfam = AlignIO.parse(args.local_pfam, 'stockholm')
+    swissprot = SeqIO.index(local_uniprot, 'swiss')
+    pfam = AlignIO.parse(local_pfam, 'stockholm')
 
-    for a in pfam:
-        pass
+    for alignment in pfam:
+        alignment_name = alignment.annotations['GF']['AC'][0]
+        if 'PF0000' in alignment_name:
+            annotate_alignment.main(alignment, alignment_name, seq_id_filter, use_local_alignment, swissprot, downloads)
 
 
 if __name__ == '__main__':
     # CLI Arguments
     parser = argparse.ArgumentParser(description='Create variant counts column wise for an alignment.')
-    parser.add_argument('--local_uniprot', type=str, help='Path to uniprot_sprot.dat')
+    parser.add_argument('--local_uniprot_file', type=str, help='Path to uniprot_sprot.dat')
     parser.add_argument('--local_pfam', type=str, help='Path to PFAM-A.seed')
+    parser.add_argument('--seq_id_filter', type=str,
+                        help='An inclusive filter to process only a subset of sequences.')
+    parser.add_argument('--use_local_alignment', action='store_true',
+                        help='Align sub-sequences to UniProt rather than enforcing exact match.')
+    parser.add_argument('--downloads', type=str, default='.VarAlign',
+                        help='A directory to store downloaded files.')
     args = parser.parse_args()
 
-    main(args)
+    local_uniprot = args.local_uniprot_file
+    local_pfam = args.local_pfam
+    seq_id_filter = args.seq_id_filter
+    use_local_alignment = args.use_local_alignment
+    downloads = args.downloads
+
+    main(local_uniprot, local_pfam, seq_id_filter, use_local_alignment, downloads)
