c99e0faaa4fc5949ef3dbca3d9f9c4de6b507e26
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2016-05-13 11:18:45 +0100

    Refactoring

---
+++ mapping.py
@@ -0,0 +1,101 @@
+import pandas as pd
+from Bio import pairwise2
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.pairwise2 import format_alignment
+
+import logging
+from utils import parse_seq_name
+
+log = logging.getLogger(__name__)
+
+
+def get_row_residue_numbers(subseq, uniprot_seq, use_local_alignment):
+    """
+    Map each sequence in an alignment to a longer sequence and return the residue numbers.
+
+    :param align: MultipleSeqAlignment for processing.
+    :param uniprot_seq: List of full-length SeqRecords.
+    :param use_local_alignment: If True, use a local alignment rather than exact pattern matching.
+    :return: A list of tuples with the sequence IDs and residues numbers for each sequence in the alignment.
+    """
+    # Prepare sequences for alignment to UniProt by removing gaps
+    subseq = SeqRecord(Seq(str(subseq.seq).replace('-', '').upper(), subseq.seq.alphabet), id=subseq.id)
+    sequence_name = parse_seq_name(subseq.id)  # TODO: No longer needed
+
+    if use_local_alignment:
+        # Align input alignment sequences to UniProt Sequences
+        log.debug('Aligning sub-sequence: {} to UniProt sequence: {}'.format(subseq.id, uniprot_seq[1].id))
+        local_alignment = pairwise2.align.localxs(subseq.seq, uniprot_seq[1].seq, -.5, -.1)
+        for pairwise_alignment in local_alignment:
+            log.debug('{}'.format('\n' + format_alignment(*pairwise_alignment)))
+        alignment = subseq.id, uniprot_seq[1].id, local_alignment
+
+        # Build list of UniProt residue numbers for each non-gap for each sequence
+        sub_seq_id, uniprot_seq_id, pairwise = alignment
+        seq = str(pairwise[0][0])
+        res_nums = [i + 1 for i, s in enumerate(seq) if s != '-']  # TODO: wrong if other seq has gaps too
+        seq_indexes = range(1, len(res_nums) + 1)
+        align_res_nums = sub_seq_id, uniprot_seq_id, res_nums, seq_indexes
+    else:
+        str_seq = str(subseq.seq)
+        uniprot_str_seq = str(uniprot_seq[1].seq)
+        if str_seq in uniprot_str_seq:
+            log.debug('Matching sub-sequence: {} to UniProt sequence: {}'.format(subseq.id, uniprot_seq[1].id))
+            start = uniprot_str_seq.find(str_seq) + 1
+            end = start + len(str_seq)
+            res_nums = range(start, end)
+            seq_indexes = range(1, len(res_nums) + 1)
+            align_res_nums = subseq.id, uniprot_seq[1].id, res_nums, seq_indexes
+        else:
+            log.error('Sub-sequence: {} does not match UniProt sequence: {}'.format(subseq.id, uniprot_seq[1].id))
+            log.error('Sub-sequence: {} does not match UniProt sequence: {}'.format(str_seq, uniprot_str_seq))
+            raise TypeError
+
+    return align_res_nums
+
+
+def get_sequence_column_numbers(sequence):
+    """
+    Build list of column numbers for each non-gap for each sequence.
+
+    :param sequence: Aligned sequence.
+    :return:
+    """
+    seq_id = sequence.id
+    seq = str(sequence.seq)
+    col_nums = [i + 1 for i, s in enumerate(seq) if s != '-']
+    align_col_nums = seq_id, col_nums
+
+    return align_col_nums
+
+
+def map_columns_to_residues(alignment_column_numbers, alignment_residue_numbers):
+    """
+    Map alignment columns to UniProt residue numbers.
+
+    :param alignment_column_numbers:
+    :param alignment_residue_numbers:
+    :return:
+    """
+    col_num_index, sequence_col_nums = zip(*alignment_column_numbers)
+    mapped = []
+    for seq_id, uniprot_seq_id, res_nums, seq_index in alignment_residue_numbers:
+        log.debug('Mapping {}...'.format(seq_id))
+        col_nums = sequence_col_nums[col_num_index.index(seq_id)]  # Lookup sequence column numbers
+        record = {'seq_id': seq_id, 'uniprot_seq_id': uniprot_seq_id, 'uniprot_res_num': res_nums,
+                  'alignment_col_num': col_nums, 'sequence_index': seq_index}
+        # TODO: This is messy due to dependency on uniprot_seq_id format in `alignment_residue_numbers`
+        if '|' in record['uniprot_seq_id']:
+            prot_name = record['uniprot_seq_id'].split('|')[1]  # UniProt ID
+        else:
+            prot_name = record['uniprot_seq_id']
+        record.update({'UniProt_ID': prot_name})
+
+        mapped.append(pd.DataFrame(record))
+
+    # Create and concat mapping tables
+    log.debug('Tabulating data...')
+    mapped_df = pd.concat(mapped, ignore_index=True)
+
+    return mapped_df
