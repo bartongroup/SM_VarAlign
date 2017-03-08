b5e738fcbb872789adc6009c77a2eff2e9d2896f
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2017-03-08 18:41:33 +0000

    Better handling of non-standard sequence regions. Now throws error if no usable mapping achieved.

--- align_variants.py
+++ align_variants.py
@@ -24,9 +24,14 @@ def _fetch_variants_for_uniprot(uniprot, canonical=True, consequences=('missense
     ensembl_ranges = [ensembl.get_genomic_range(x) for x in ensembl_genes]
     for i in range(len(ensembl_genes)):
         log.info('Mapped {} to {} on chr: {}, {}-{}'.format(uniprot, ensembl_genes[i], *ensembl_ranges[i]))
-    ensembl_ranges = [x for x in ensembl_ranges if x[0] in ensembl.standard_regions]
-    assert len(ensembl_ranges) >= 1
-    log.info('Filtered ranges: %s', ensembl_ranges)
+    non_standard_ranges = [i for i, x in enumerate(ensembl_ranges) if x[0] not in ensembl.standard_regions]
+    if len(non_standard_ranges) > 0:
+        non_standard_ranges.sort(reverse=True)
+        [ensembl_genes.pop(i) for i in non_standard_ranges]
+        [ensembl_ranges.pop(i) for i in non_standard_ranges]
+        log.info('Removed %s non-standard sequence regions.', len(non_standard_ranges))
+    if len(ensembl_ranges) == 0:
+        raise ValueError('Could not map {} to the genome.'.format(uniprot))
 
     # Lookup variants
     log.info('Retrieving variants...')
