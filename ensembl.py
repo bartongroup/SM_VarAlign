2f9a1c05357fb85f763d4653c4b3bda2413ff1fc
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2017-03-08 20:56:46 +0000

    Function to merge overlapping or nearly overlapping ranges.

--- ensembl.py
+++ ensembl.py
@@ -40,3 +40,29 @@ def get_genomic_range(query_id, server=default_server):
     decoded = r.json()
 
     return str(decoded['seq_region_name']), decoded['start'], decoded['end']
+
+
+def merge_ranges(ranges, min_gap=150):
+    """
+    Merge a set of genomic ranges into non-overlapping sets.
+
+    :param ranges:
+    :param min_gap: Minimum gap to allow between consecutive ranges.
+    :return:
+    """
+    ranges = ranges[:]
+    ranges.sort(key=lambda a: a[2])  # Sort by end
+    ranges.sort(key=lambda a: a[1])  # Sort by start
+    ranges.sort(key=lambda a: a[0])  # Sort by region
+
+    new_ranges = [list(ranges.pop(0))]
+    for region, start, end in ranges:
+        if region == new_ranges[-1][0]:
+            if start <= new_ranges[-1][2] + min_gap:
+                new_ranges[-1][2] = end  # Merge range
+            else:
+                new_ranges.append([region, start, end])  # New range on same region
+        else:
+            new_ranges.append([region, start, end])  # New range on different region
+
+    return [tuple(x) for x in new_ranges]
