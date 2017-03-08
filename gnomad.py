7564b5a43bfcffb7d69dfa9d4e9a173295901a2e
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2017-03-08 14:14:43 +0000

    Added function to split multialleleic variants into single variants.

--- gnomad.py
+++ gnomad.py
@@ -1,6 +1,7 @@
 import itertools
 
 from config import defaults
+from copy import deepcopy
 import logging
 import pandas as pd
 import vcf  ## Requires pysam functionality
@@ -47,3 +48,40 @@ def tabulate_variant_effects(variants):
     vep_table = pd.DataFrame(list(itertools.chain.from_iterable(veps)),
                              columns=CSQ_Format, index=variant_indices)
     return vep_table
+
+
+def split_variant(variant, alleles=[], exclude=('CSQ',)):
+    """
+    Split a multiallelic variant.
+
+    :param variant:
+    :param alleles: Alleles to be extracted, either by ALT allele or ALT allele index (default: all alleles).
+    :param exclude: INFO fields to be dropped from the new variant records.
+    :return:
+    """
+    def _extract_allele(variant, allele_index, exclude):
+        new_variant = deepcopy(variant)
+        # ALT field
+        new_variant.ALT = [new_variant.ALT[allele_index], ]
+        # INFO field
+        info = new_variant.INFO
+        [info.pop(x, 0) for x in exclude]  # Exclude ignored info fields
+        new_variant.INFO = {k: [v[allele_index], ] if isinstance(v, list) else v for k, v in info.items()}
+        return new_variant
+
+    # Work out alleles to process, either all or selection by ALT allele base or index
+    if alleles == ():
+        alleles = range(len(variant.ALT))
+    else:
+        if isinstance(alleles, int):
+            pass
+        if isinstance(alleles[0], str):  # Will work with single string or list
+            alleles = [variant.ALT.index(x) for x in alleles]
+
+    # Extract desired alleles
+    allelic_records = []
+    for i in alleles:
+        new_variant = _extract_allele(variant, i, exclude)
+        allelic_records.append(new_variant)
+
+    return allelic_records
