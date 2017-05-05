12f90ed5fd67b4178f644503a66ac0fc1ec6a96e
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2017-05-05 15:22:47 +0100

    Add easy caching of Ensembl requests

--- varalign/ensembl.py
+++ varalign/ensembl.py
@@ -1,9 +1,11 @@
 from config import defaults
 import requests
+import requests_cache
 import sys
 
 
 default_server = defaults.api_ensembl
+requests_cache.install_cache('ensembl_cache')
 
 standard_regions = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
                     '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
