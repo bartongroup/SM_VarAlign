90ef0e1ce368e600f0a9568ab4703f63fcff4982
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2017-08-03 12:36:35 +0100

    Added option to aggregate by any column/ index combination to `_aggregate_annotation`.

--- varalign/analysis_toolkit.py
+++ varalign/analysis_toolkit.py
@@ -1,11 +1,16 @@
 
 
-def _aggregate_annotation(aligned_variant_table, annotation_column, fill_value=0):
+def _aggregate_annotation(aligned_variant_table, annotation_column, aggregate_by=[('Alignment', 'Column')],
+                          fill_value=0):
     """
+    Create frequency table for a categorical column over other columns and/or index levels.
 
-    :param aligned_variant_table:
-    :param annotation_column:
-    :return:
+    :param aligned_variant_table: DataFrame
+    :param annotation_column: Column name containing categories to count.
+    :param aggregate_by: List of column and/or index names to group by.
+    :param fill_value: Value to fill unobserved categories.
+    :return: DataFrame of counts indexed by `aggregate_by` with the unique values of `annotation_column` as columns.
     """
-    grouped = aligned_variant_table.groupby([('Alignment', 'Column'), annotation_column])
+    groupby = aggregate_by + [annotation_column]
+    grouped = aligned_variant_table.groupby(groupby)
     return grouped.size().unstack(fill_value=fill_value)
