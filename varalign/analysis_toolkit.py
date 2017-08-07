315a6c3af40dba85856e8887fc5f68fb918a6296
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2017-08-07 14:33:10 +0100

    Added function to calculate column variant scores.

--- varalign/analysis_toolkit.py
+++ varalign/analysis_toolkit.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from scipy.stats import linregress
+from scipy import stats
 
 
 def _aggregate_annotation(aligned_variant_table, annotation_column, aggregate_by=[('Alignment', 'Column')],
@@ -30,19 +30,42 @@ def _comparative_regression(column_variant_counts, filter_mask=None, regressor =
     """
     regressions = []
     # Regressions for all columns
-    regressions.append(linregress(x=column_variant_counts[regressor],
-                                  y=column_variant_counts['missense_variant'])._asdict())
-    regressions.append(linregress(x=column_variant_counts[regressor],
-                                  y=column_variant_counts['synonymous_variant'])._asdict())
+    regressions.append(stats.linregress(x=column_variant_counts[regressor],
+                                        y=column_variant_counts['missense_variant'])._asdict())
+    regressions.append(stats.linregress(x=column_variant_counts[regressor],
+                                        y=column_variant_counts['synonymous_variant'])._asdict())
     row_names = ['missense', 'synonymous']
     if filter_mask is not None:
         # Regressions for filtered columns
-        regressions.append(linregress(x=column_variant_counts[filter_mask][regressor],
-                                      y=column_variant_counts[filter_mask]['missense_variant'])._asdict())
-        regressions.append(linregress(x=column_variant_counts[filter_mask][regressor],
-                                      y=column_variant_counts[filter_mask]['synonymous_variant'])._asdict())
+        regressions.append(stats.linregress(x=column_variant_counts[filter_mask][regressor],
+                                            y=column_variant_counts[filter_mask]['missense_variant'])._asdict())
+        regressions.append(stats.linregress(x=column_variant_counts[filter_mask][regressor],
+                                            y=column_variant_counts[filter_mask]['synonymous_variant'])._asdict())
         row_names += ['filtered_missense', 'filtered_synonymous']
 
     results = pd.DataFrame(regressions, index=row_names)
     results.columns.name = regressor
     return results
+
+
+def _column_variant_scores(column_variant_counts, variant_class='missense_variant'):
+    """Calculate variation scores from a DataFrame of column variant totals.
+
+    :param column_variant_counts:
+    :param variant_class:
+    :return:
+    """
+    # column_data.query('Human_res_occupancy >= 5', inplace=True)
+    alignment_totals = column_variant_counts.loc[:, [variant_class, 'Human_res_occupancy']].sum()
+
+    # Calculate missense scores
+    missense_scores = []
+    for mis_col, occ_col in column_variant_counts[[variant_class, 'Human_res_occupancy']].itertuples(index=False):
+        mis_other = alignment_totals[variant_class] - mis_col
+        occ_other = alignment_totals['Human_res_occupancy'] - occ_col
+        oddsratio, pvalue = stats.fisher_exact([[mis_col, mis_other], [occ_col, occ_other]])
+        missense_scores.append((oddsratio, pvalue))
+
+    # Parse to dataframe
+    return pd.DataFrame(missense_scores, columns=['oddsratio', 'pvalue'], index=column_variant_counts.index)
+
