357a6e23a336aca8165bd5789eaaed2ff33f7bb9
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2017-08-07 14:19:12 +0100

    Add function to make table of regression parameters for (filtered)missense/ (filtered)synonymous vs. another column variable.

--- varalign/analysis_toolkit.py
+++ varalign/analysis_toolkit.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from scipy.stats import linregress
 
 
 def _aggregate_annotation(aligned_variant_table, annotation_column, aggregate_by=[('Alignment', 'Column')],
@@ -16,3 +18,31 @@ def _aggregate_annotation(aligned_variant_table, annotation_column, aggregate_by
     groupby = aggregate_by + [annotation_column]
     grouped = aligned_variant_table.groupby(groupby)
     return grouped.size().unstack(fill_value=fill_value)
+
+
+def _comparative_regression(column_variant_counts, filter_mask=None, regressor ='Human_res_occupancy'):
+    """Return table of regression parameters for missense / synonymous counts vs. a regressor.
+
+    :param column_variant_counts:
+    :param filter_mask:
+    :param regressor:
+    :return:
+    """
+    regressions = []
+    # Regressions for all columns
+    regressions.append(linregress(x=column_variant_counts[regressor],
+                                  y=column_variant_counts['missense_variant'])._asdict())
+    regressions.append(linregress(x=column_variant_counts[regressor],
+                                  y=column_variant_counts['synonymous_variant'])._asdict())
+    row_names = ['missense', 'synonymous']
+    if filter_mask is not None:
+        # Regressions for filtered columns
+        regressions.append(linregress(x=column_variant_counts[filter_mask][regressor],
+                                      y=column_variant_counts[filter_mask]['missense_variant'])._asdict())
+        regressions.append(linregress(x=column_variant_counts[filter_mask][regressor],
+                                      y=column_variant_counts[filter_mask]['synonymous_variant'])._asdict())
+        row_names += ['filtered_missense', 'filtered_synonymous']
+
+    results = pd.DataFrame(regressions, index=row_names)
+    results.columns.name = regressor
+    return results
