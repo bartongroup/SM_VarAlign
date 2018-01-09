32fca12d1f8035cb8f3957795eb0faca0e1325b4
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2018-01-09 10:07:43 +0000

    Added metadata to __init__.py and improved setup.py.

--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+from varalign import __version__, __license__
+
+
+def gather_dependencies():
+    with open('requirements.txt', 'r') as f_in:
+        return [l for l in f_in.read().rsplit(os.linesep)
+                if l and not l.startswith("#")]
+DEPENDENCIES = gather_dependencies()
 
 
 setup(
     name='VarAlign',
-    version='',
-    packages=['varalign', 'varalign.misc'],
-    #data_files=[('config', ['varalign/config.txt'])],
+    version=__version__,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    # data_files=[('config', ['varalign/config.txt'])],
     package_data={'varalign': ['config.txt']},
-    #include_package_data=True,
+    # include_package_data=True,
     scripts=['bin/filter_swiss'],
-    install_requires=['requests', 'pandas', 'pyvcf', 'pysam', 'scipy'],
-    url='',
-    license='',
-    author='smacgowan',
+    install_requires=DEPENDENCIES,
+    url='https://github.com/stuartmac/VarAlign/',
+    license=__license__,
+    author='Stuart MacGowan',
     author_email='s.macgowan@dundee.ac.uk',
     description='This package is used to map and aggregate variants in multiple sequence alignments.'
 )
---
+++ varalign/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__ = 'varalign'
+__version__ = '0.0.0'
+__license__ = 'MIT'
+__authors__ = 'Stuart MacGowan'
