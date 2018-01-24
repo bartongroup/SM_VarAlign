d35e94c2e0159650e376de46f41b6c0d850aeb05
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2018-01-24 15:55:57 +0000

    Add module level logging.

--- varalign/__init__.py
+++ varalign/__init__.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import logging
+
+logging.getLogger("varalign").addHandler(logging.NullHandler())
+logging.captureWarnings(True)
+logging.basicConfig(stream=sys.stderr, level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+
+
 __title__ = 'varalign'
 __version__ = '0.0.0'
 __license__ = 'MIT'
