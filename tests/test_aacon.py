73a6ef1ebf4d13ff60fc36b3f78645130ae5c15a
Stuart MacGowan <s.macgowan@dundee.ac.uk>
2018-10-30 17:49:16 +0000

    Add AACon interface test (#6)

---
+++ tests/test_aacon.py
@@ -0,0 +1,22 @@
+import os
+
+from unittest import TestCase
+
+from Bio import AlignIO
+from varalign import aacon
+
+
+class TestGet_aacon(TestCase):
+    def test_aacon(self):
+        # Reformat the test alignment
+        alignment_path = os.path.join(os.path.dirname(__file__), 'data', 'issues', '006', 'peptide_fJ_with_ACCs.sto')
+        alignment = AlignIO.read(alignment_path, 'stockholm')
+        reformatted, _ = aacon._reformat_alignment_for_aacon(alignment)
+
+        # Extract first sequence from each alignment
+        s1 = str(alignment[0].seq).replace('-', '')
+        s2 = str(reformatted[0].seq).replace('-', '')
+
+        # Check they are equal
+        message = 'Alignment reformatting has altered sequences.'
+        self.assertEqual(s1, s2, message)
